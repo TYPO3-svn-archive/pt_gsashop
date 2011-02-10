6c7e69ee1fa581b38b83d8ff251a1bdca93de090
daniellienert <daniellienert@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-10 13:33:39 +0000

    CHG: get DispatchCost from delivery Object instead of dispatchObject
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pt_gsashop/trunk@43393 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/class.tx_ptgsashop_order.php
+++ res/class.tx_ptgsashop_order.php
@@ -255,7 +255,7 @@ class tx_ptgsashop_order {
                 
             // insert delivery's dispatch cost record
             $artSumTotalArr[$delKey] = $delObj->get_articleCollObj()->getItemsTotal($this->get_isNet());
-            $orderAccessor->insertOrdersDispatchCost($delObj->get_dispatchObj(), $pid, $this->feCrUserId, $this->orderArchiveId, $delRecordIdArr[$delKey], $artSumTotalArr[$delKey]);
+            $orderAccessor->insertOrdersDispatchCost($delObj, $pid, $this->feCrUserId, $this->orderArchiveId, $delRecordIdArr[$delKey], $this->get_isNet());
             
             // insert delivery's article records
             foreach ($delObj->get_articleCollObj() as $articleObj) {
@@ -323,8 +323,7 @@ class tx_ptgsashop_order {
             $orderAccessor->updateOrdersAddress($delObj->get_shippingAddrObj(), $this->orderArchiveId, $delObj->get_orderArchiveId());
                 
             // update delivery's dispatch cost record
-            $deliveryArticlesSumTotal = $delObj->get_articleCollObj()->getItemsTotal($this->get_isNet());
-            $orderAccessor->updateOrdersDispatchCost($delObj->get_dispatchObj(), $this->orderArchiveId, $delObj->get_orderArchiveId(), $deliveryArticlesSumTotal);
+            $orderAccessor->updateOrdersDispatchCost($delObj, $this->orderArchiveId, $this->get_isNet());
             
             // update delivery's article records
             foreach ($delObj->get_articleCollObj() as $articleObj) {
@@ -574,7 +573,6 @@ class tx_ptgsashop_order {
         
         // round return sum to 2 decimal digits 
         $dispatchSumTotalRounded = round((double)$dispatchSumTotal, 2);
-        
         return (double)$dispatchSumTotalRounded;
         
     }
